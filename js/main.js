e8643a5ea902bee4a12410f346878dc882fe6c55
daniyeyo <118698666+daniyeyo@users.noreply.github.com>
2022-12-07 17:42:18 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,28 @@ $(function () {
     $('button').on('click', function () {
         $('h1').toggleClass('on')
     });
-    $('.mainSlide').slick()
+    $('.mainSlide').slick({
+        dots: true,
+        autoplay: true,
+        trueauopSpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
 
+
+})
+
+$('h1').on('click', function () {
+    console.log('작동함')
+})
+
+
+$(window).on('scroll', function () {
+    console.log('스크롤 :', $(window).scrollTop())
+
+    if ($(window).scrollTop() > 0) {
+        $('.header_scroll').addClass('on')
+    } else {
+        $('.header_scroll').removeClass('on')
+    }
 })
